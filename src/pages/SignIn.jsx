b1bbab0be05282b1b1f2ba5662230378105aea10
sadmin23 <sadmid26tahmid@gmail.com>
2024-09-02 23:03:21 +0600

    Add gauge chart

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
+import SignUp from './SignUp'
 
 export default function SignIn() {
   return (
-    <div className='bg-sky-950 w-full min-h-screen px-60 py-24'>
+    <div className='bg-sky-950 w-full min-h-screen  py-24'>
+      <header className='px-60'>
       <h1 className='text-6xl font-semibold text-white text-center'>Discover Your Sentiment Instantly</h1>
       <h2 className='text-xl mt-10 text-white text-center'>
         Analyze social media posts and reviews with a single click. 
         Get quick and accurate sentiment scores to understand emotions behind the words — positive, negative, or neutral.
       </h2>
-      <section className='w-2/3 bg-white rounded-xl mt-10'>
+      </header>
+      <div className='flex border-white mt-10 pl-52 pr-40'>
+      <section className='w-3/5 bg-white rounded-xl'>
         <textarea 
           className='w-full p-4 rounded-t-lg border-t-2 h-80 resize-none align-top' 
           placeholder='Enter your text here'
-        />
+          />
         <div className='flex justify-end border-t-2 py-3 pr-2'>
           <button className='bg-teal-600 text-lg text-white font-semibold rounded-2xl py-2 px-6'>
             Analyze
           </button>
         </div>
       </section>
+      <SignUp />
+      </div>
     </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,28 +1,61 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import GaugeComponent from 'react-gauge-component';
 
 export default function SignUp() {
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleRegister = () => {
-    console.log(email, password);
-  };
-
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-bold my-7'>Sign Up</h1>
-      <div className='flex flex-col gap-4'>
-        <input className='border p-3 rounded-lg' type="text" placeholder="Username" id='username' />
-        <input className='border p-3 rounded-lg' type="text" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input className='border p-3 rounded-lg' type="password" placeholder="Enter a Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <button className='bg-blue-600 text-white p-3 rounded-lg text-lg font-semibold hover:opacity-100' onClick={handleRegister}>Sign Up</button>
-      </div>
-      <div className='flex gap-1 mt-5 font-semibold'>
-        <p>Have an account?</p>
-        <Link to='/sign-in' className='text-blue-800 hover:underline'>Sign In</Link>
-      </div>
-    </div>
+<GaugeComponent
+  style={{ width: '40%', height: '100px'}} // Example dimensions
+  className='ml-10 mt-8'
+  type="semicircle"
+  arc={{
+    width: 0.2,
+    padding: 0.005,
+    cornerRadius: 1,
+    subArcs: [
+      {
+        limit: -0.2,
+        color: '#EA4228',
+        showTick: true,
+        tooltip: {
+          text: 'Negtive!'
+        },
+        onClick: () => console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+        onMouseMove: () => console.log("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"),
+        onMouseLeave: () => console.log("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"),
+      },
+      {
+        limit: 0.2,
+        color: '#F5CD19',
+        showTick: true,
+        tooltip: {
+          text: 'Neutral!'
+        }
+      },
+      {
+        limit: 1,
+        color: '#5BE12C',
+        tooltip: {
+          text: 'Positive!'
+        }
+      }
+    ]
+  }}
+  pointer={{
+    color: '#345243',
+    length: 0.70,
+    width: 15,
+    elastic: true,
+  }}
+  labels={{
+    valueLabel: { formatTextValue: value => value, fontSize: 10 },
+    tickLabels: {
+      type: 'outer',
+      valueConfig: { formatTextValue: value => value, fontSize: 10 }
+    }
+  }}
+  value={0.42}
+  minValue={-1}
+  maxValue={1}
+/>
   )
 }
