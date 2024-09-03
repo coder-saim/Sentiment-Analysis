0e6a6d567bfbd94f05a356f85e3d046b8960305e
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-09-03 20:04:21 +0600

    database model added

---
+++ backend/app/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    role = Column(String, index=True)
+    name = Column(String, index=True, nullable=True)
+
+
+
