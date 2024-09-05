ddcde7ab87e2895fe6953a091fd1b665213804e8
sadmin23 <sadmid26tahmid@gmail.com>
2024-09-05 23:17:17 +0600

    final ui update

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import background from '../assets/Background.png'
+import logo from '../assets/logo.png'
 
 export default function Hero() {
   return (
     <div className='w-full relative flex justify-center overflow-hidden'>
       <header className='absolute top-0 left-0 w-full flex justify-between items-center py-6 bg-transparent px-60'>
-        <h1 className='text-white text-2xl font-bold'>SentiScan</h1>
+        <div className='flex items-center space-x-3'>
+          <img src={logo} alt='logo' className='w-16 h-16 object-cover object-center' />
+          <h1 className='text-white text-2xl font-bold'>SentiScan</h1>
+        </div>
         <nav className='flex space-x-8 text-white'>
           <a href='#get-started' className='text-lg font-semibold border-2 p-3 w-32 text-center rounded-2xl hover:font-bold'>Get Started</a>
         </nav>
@@ -16,11 +20,11 @@ export default function Hero() {
         className='w-[2688px] max-w-none h-[420px] object-cover object-center'
       />
       <h1 className='absolute top-48 transform -translate-y-1/2 text-white text-5xl font-bold'>
-        Discover Your Sentiment
+        AI-Driven Sentiment Analysis
       </h1>
       <h2 className='px-80 text-center absolute top-[300px] transform -translate-y-1/2 text-white text-xl font-semibold'>
-      Analyze social media posts and reviews with a single click. 
-      Get quick and accurate sentiment scores to understand emotions behind the words —positive, negative, or neutral.
+      Harness the power of AI to analyze social media posts and reviews instantly. 
+      Get accurate sentiment insights—positive, negative, or neutral—with just a single click.
       </h2>      
     </div>
   )
