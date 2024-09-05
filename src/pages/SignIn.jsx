25882a317dd0a6abecc4690904b90c3f69a3ebb8
sadmin23 <sadmid26tahmid@gmail.com>
2024-09-05 12:45:52 +0600

    create new hero section

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,33 +1,27 @@
 import React from 'react'
-import SignUp from './SignUp'
+import background from '../assets/Background.png'
 
 export default function SignIn() {
   return (
-    <div className='bg-sky-950 w-full min-h-screen  py-24'>
-      <header className='px-96'>
-      <h1 className='text-6xl font-semibold text-white text-center'>Discover Your Sentiment</h1>
-      <h2 className='text-xl mt-10 text-white text-center'>
-        Analyze social media posts and reviews with a single click. 
-        Get quick and accurate sentiment scores to understand emotions behind the words.</h2>
+    <div className='w-full min-h-screen relative flex justify-center overflow-hidden'>
+      <header className='absolute top-0 left-0 w-full flex justify-between items-center py-6 bg-transparent px-60'>
+        <h1 className='text-white text-3xl font-bold'>SentiScan</h1>
+        <nav className='flex space-x-8 text-white'>
+          <a href='#get-started' className='text-lg font-semibold border-2 p-3 rounded-2xl'>Get Started</a>
+        </nav>
       </header>
-      <div className='flex mt-16 pl-52 pr-40'>
-      <section className='w-3/5 bg-white rounded-xl'>
-        <textarea 
-          className='w-full p-4 rounded-t-lg border-t-2 h-60 resize-none align-top' 
-          placeholder='Enter your text here'
-          />
-        <div className='flex justify-end border-t-2 py-2 pr-2'>
-          <button className='bg-teal-600 text-lg text-white font-semibold rounded-2xl py-2 px-6'>
-            Analyze
-          </button>
-        </div>
-      </section>
-        <div>
-          <h1 className='font-semibold text-3xl ml-6 text-white text-center tracking-wide'>Sentiment Score</h1>
-          <SignUp />
-          <h1 className='font-semibold text-3xl ml-6 mt-4 text-white text-center tracking-wide'>Sentiment : Nuetral</h1>
-        </div>
-      </div>
+      <img 
+        src={background} 
+        alt='hero' 
+        className='w-[2688px] max-w-none h-[450px] object-cover object-center'
+      />
+      <h1 className='absolute top-48 transform -translate-y-1/2 text-white text-5xl font-bold'>
+        Discover Your Sentiment
+      </h1>
+      <h2 className='px-96 text-center absolute top-72 transform -translate-y-1/2 text-white text-xl font-semibold'>
+      Analyze social media posts and reviews with a single click. 
+      Get quick and accurate sentiment scores to understand emotions behind the words.
+      </h2>      
     </div>
   )
 }
